cd6b5b98a798c7e3aacb57daca04f1652e531903
joshuaoliver <josh@bywave.com.au>
2023-09-21 13:46:28 +1000

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,8 +4,8 @@ use li3_flash_message\extensions\storage\FlashMessage;
 use lithium\action\Dispatcher;
 use lithium\aop\Filters;
 
-\lithium\aop\Filters::apply('\lithium\action\Dispatcher','_callable', function($self, $params, $chain) {
-	$object = $chain->next($self, $params, $chain);
+\lithium\aop\Filters::apply('\lithium\action\Dispatcher','_callable', function( $params, $next) {
+	$object = $next($params);
 
 	if (is_a($object, 'lithium\action\Controller')) {
 		return FlashMessage::bindTo($object);
